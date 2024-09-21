69f8ea7ba85ed21fd46c80dbd92e98f4cf390d38
Muhammad Zain <mzain.akhtar3@gmail.com>
2024-09-21 22:12:33 +0500

    location added

--- frontend/src/components/LocationPicker.jsx
+++ frontend/src/components/LocationPicker.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMapEvents, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const LocationPicker = ({ pickLocation, lat, lng, currentLocation }) => {
@@ -48,7 +48,9 @@ const LocationPicker = ({ pickLocation, lat, lng, currentLocation }) => {
 				zoom={13}
 				style={{ height: "400px", width: "100%" }}>
 				<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-				<Marker position={position} />
+				<Marker position={position} >
+					<Popup>Latitude: {position[0]}<br/>Longitude: {position[1]}</Popup>
+				</Marker>
 				<HandleMapClick /> {/* Enable click functionality on the map */}
 			</MapContainer>
 
